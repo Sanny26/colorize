f258920f3e617de35219f2e1774dd999e218d094
Chris Andrew <chris.g14@iiits.in>
2018-03-28 20:21:10 +0530

    Update loss functions.

--- clrmdl.py
+++ clrmdl.py
@@ -1,6 +1,8 @@
-from keras.applications.vgg16 import VGG16 
+"""CNN to colorize grayscale images."""
+
+from keras.applications.vgg16 import VGG16
 from keras.models import Model, model_from_json
-from keras.layers import Input, Lambda, Add
+from keras.layers import Input, Add
 from keras.layers import BatchNormalization, Conv2D, UpSampling2D, concatenate
 from keras.preprocessing import image
 from keras import backend as K
@@ -13,13 +15,14 @@ import numpy as np
 import sys
 import cv2
 
+
 def dist(x, y):
-    """
-    Euclidean distance loss
-    """
+    """Euclidean distance loss."""
     return K.sqrt(K.sum(K.square(y - x)))
 
+
 def blur(img, kernel_size):
+    """Apply gaussian blurring to the image."""
     kernel_x = cv2.getGaussianKernel(kernel_size, sigma=4)
     kernel_y = cv2.getGaussianKernel(kernel_size, sigma=4)
     kernel = kernel_x*np.transpose(kernel_y)
@@ -32,10 +35,12 @@ def blur(img, kernel_size):
 
     return filtered_image
 
+
 def rgb2uv(img):
-    rgb_to_yuv_kernel = [[0.299, -0.14714119,
-                       0.61497538], [0.587, -0.28886916, -0.51496512],
-                        [0.114, 0.43601035, -0.10001026]]
+    """Convert rgb image to yuv space."""
+    rgb_to_yuv_kernel = [[0.299, -0.14714119, 0.61497538],
+                         [0.587, -0.28886916, -0.51496512],
+                         [0.114, 0.43601035, -0.10001026]]
     kernel = tf.convert_to_tensor(rgb_to_yuv_kernel, dtype=tf.float32)
     ndims = img.get_shape().ndims
 
@@ -44,22 +49,22 @@ def rgb2uv(img):
 
 
 def blur_uv_loss(rgb, inferred_rgb):
-  uv = rgb2uv(rgb)
-  uv_blur0 = rgb2uv(blur(rgb, 3))
-  uv_blur1 = rgb2uv(blur(rgb, 5))
+    """Calculate custom loss."""
+    uv = rgb2uv(rgb)
+    uv_blur0 = rgb2uv(blur(rgb, 3))
+    uv_blur1 = rgb2uv(blur(rgb, 5))
 
-  inferred_uv = rgb2uv(inferred_rgb)
-  inferred_uv_blur0 = rgb2uv(blur(inferred_rgb, 3))
-  inferred_uv_blur1 = rgb2uv(blur(inferred_rgb, 5))
-  #pdb.set_trace()
-  loss = ( dist(inferred_uv, uv) +
-            dist(inferred_uv_blur0 , uv_blur0) +
-            dist(inferred_uv_blur1, uv_blur1) ) / 3
-  print(loss)
-  return  loss
+    inferred_uv = rgb2uv(inferred_rgb)
+    inferred_uv_blur0 = rgb2uv(blur(inferred_rgb, 3))
+    inferred_uv_blur1 = rgb2uv(blur(inferred_rgb, 5))
+    loss = (dist(inferred_uv, uv) +
+            dist(inferred_uv_blur0, uv_blur0) +
+            dist(inferred_uv_blur1, uv_blur1))/3
+    return loss
 
 
 def batch_generator(X, y, batch_size):
+    """Generate batches for training."""
     Xb_shape = list(X.shape)
     Xb_shape[0] = batch_size
     X_batch = np.zeros(tuple(Xb_shape))
@@ -77,54 +82,58 @@ def batch_generator(X, y, batch_size):
 
 
 def define_model():
-    input_image = Input(shape= (224, 224, 1) )
+    """CNN model definition."""
+    input_image = Input(shape=(224, 224, 1))
     img = concatenate([input_image, input_image], axis=-1)
-    img = concatenate([img, input_image], axis = -1)
+    img = concatenate([img, input_image], axis=-1)
 
-    vgg_model = VGG16(include_top=False, weights= "imagenet", input_tensor = img) #Y 
+    vgg_model = VGG16(include_top=False, weights="imagenet", input_tensor=img)  # Y
 
     for layer in vgg_model.layers:
-        layer.trainable = False 
+        layer.trainable = False
 
-    x = BatchNormalization(name = "bn1" )(vgg_model.get_layer(name="block4_conv3").output)
-    x = Conv2D(256, (1, 1), activation="relu", name= "conv1" )(x)
-    up1 = UpSampling2D(size = (2,2))(x)
-    x1 = BatchNormalization(name = "bn2")(vgg_model.get_layer(name="block3_conv3").output)
+    x = BatchNormalization(name="bn1")(vgg_model.get_layer(name="block4_conv3").output)
+    x = Conv2D(256, (1, 1), activation="relu", name="conv1")(x)
+    up1 = UpSampling2D(size=(2, 2))(x)
+    x1 = BatchNormalization(name="bn2")(vgg_model.get_layer(name="block3_conv3").output)
     x = Add()([x1, up1])
-    x = Conv2D(128, (1, 1), activation="relu", name="conv2" )(x)
-    up2 = UpSampling2D(size = (2,2))(x)
+    x = Conv2D(128, (1, 1), activation="relu", name="conv2")(x)
+    up2 = UpSampling2D(size=(2, 2))(x)
     x2 = BatchNormalization(name="bn3")(vgg_model.get_layer(name="block2_conv2").output)
     x = Add()([x2, up2])
-    x = Conv2D(64, (1, 1), activation="relu", name="conv3" )(x)
-    up3 = UpSampling2D(size = (2,2))(x)
+    x = Conv2D(64, (1, 1), activation="relu", name="conv3")(x)
+    up3 = UpSampling2D(size=(2, 2))(x)
     x3 = BatchNormalization(name="bn4")(vgg_model.get_layer(name="block1_conv2").output)
     x = Add()([x3, up3])
-    x = Conv2D(3, (1, 1), activation="relu", name="conv4" )(x)
-    uv = Conv2D(2, (1,1), activation="relu", name="conv5")(x) ##UV output
-    pred_img = concatenate([input_image, uv], axis = -1)
+    x = Conv2D(3, (1, 1), activation="relu", name="conv4")(x)
+    uv = Conv2D(2, (1, 1), activation="relu", name="conv5")(x)  # UV output
+    pred_img = concatenate([input_image, uv], axis=-1)
 
-
-    model = Model(inputs = input_image, outputs= pred_img)
+    model = Model(inputs=input_image, outputs=pred_img)
     return model
 
 
 if __name__ == "__main__":
-    input_path = "input/"
-    output_path = "output/"
+    input_path = "/home/chrizandr/colorize/input/"
+    output_path = "/home/chrizandr/colorize/output/"
     files = set(os.listdir(input_path)).intersection(set(os.listdir(output_path)))
 
     X = np.empty((len(files), 224, 224, 1))
     y = np.empty((len(files), 224, 224, 3))
     for i, f in enumerate(files):
-        img = image.load_img(input_path+f, grayscale = True, target_size=(224, 224))
-        x = image.img_to_array(img)
-        X[i]=x
+        img = cv2.imread(input_path+f, 0)
+        x = cv2.resize(img, (224, 224))
+        x = np.expand_dims(x,-1)
+        X[i] = x
 
-        pred_img = image.load_img(output_path + f, target_size=(224, 224))
-        x = image.img_to_array(img)
-        y[i]= x
-        
-    if len(sys.argv)==2:
+        pred_img = cv2.imread(output_path+f)
+        try:
+           x = cv2.resize(pred_img, (224, 224))
+        except:
+           print(f)
+        y[i] = x
+
+    if len(sys.argv) == 2:
         json_file = open('model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
@@ -134,30 +143,28 @@ if __name__ == "__main__":
     else:
         model = define_model()
 
-    #pdb.set_trace()
-    model.compile(loss = blur_uv_loss,
-                    optimizer = 'adam',
-                    metrics=['accuracy'])
-    model.fit_generator(batch_generator(X, y, 3), steps_per_epoch=10, epochs= 50 ) 
+    model.compile(loss=blur_uv_loss,
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    #model.fit_generator(batch_generator(X, y, 3), steps_per_epoch=30, epochs=1500)
 
-    '''test_path = "test/"
+    test_path = "test/"
     files = os.listdir(test_path)
     timages = np.zeros((len(files), 224, 224, 1))
     for i, f in enumerate(files):
-        img = image.load_img(test_path+f, grayscale=True, target_size=(224, 224))
-        timages[i] = image.img_to_array(img)
+        img = cv2.imread(test_path+f, 0)
+        x = cv2.resize(img, (224, 224))
+        x = np.expand_dims(x, -1)
+        timages[i] = x
 
-    pimages = model.predict(timages)    
+
+    pimages = model.predict(timages)
     for i in range(len(files)):
-        plt.imshow(pimages[i])
-        plt.show()'''
-    
+        plt.imshow(np.ndarray.astype(pimages[i], np.uint8))
+        plt.show()
+
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
         json_file.write(model_json)
     model.save_weights("model.h5")
     print("Saved model to disk")
-    
-
-
-
